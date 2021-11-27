a10f43383d54a084cc98f9d1bd6666fba6914757
baliram123giri <gir71401@gmail.com>
2021-11-27 22:16:32 +0530

    String interpolation

---
+++ class_first.py
@@ -0,0 +1,10 @@
+ #comment
+name = "baliram"
+name = "Unam" # overwrite
+name2 = "Sachin Giri"
+name4 = "Vaishnavi"
+name5 = "Rohit"
+print("Hello " + name + " How Are You ?")
+print(f"hello {name2} How Are {name} You? "+f"{name2}")
+print("hello %s How are You ?"%(name2))
+print("Hello {}, How Are You ? {}".format(name4, name5))
---
+++ news_assignment.py
@@ -0,0 +1,10 @@
+word1 = "outrage"
+word2 = "statement"
+word3 = "Amazon"
+word4 = "employment"
+word5 = "pressure"
+word6 = "discontent"
+space = " "
+print("The public %s at this crass and true %s was huge and justified."%(word1,word2)+ 
+f"  {word3} is notorious for cutting workers out, be that of a collective say at work, of an {word4 + space+  word5} contract or even of a toilet break. " +
+" Work {} is high, workers’ every move is monitored, and any hint of {} is met with fierce repression.".format(word5,word6))
